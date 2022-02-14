7f7ba2f6eee623610a1e57f28c0a0311b1f13a39
Xiao Xiao <grace97909@gmail.com>
2022-02-14 00:03:47 -0500

    latest work

---
+++ MovieList/Migrations/20220214041405_Genres.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieList.Migrations
+{
+    public partial class Genres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "Genres");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Genres_GenreId",
+                table: "Movies",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "GenreId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genres_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "GenreId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
